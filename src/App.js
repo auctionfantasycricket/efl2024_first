e2c92829a660f986ddcc45f167d27f6fe1767e1a
shallymysore <bkshashankhr@gmail.com>
2025-04-09 20:10:34 -0700

    float button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Route, Routes, HashRouter} from 'react-router-dom'
+import {Route, Routes, HashRouter, useNavigate} from 'react-router-dom'
 import { NavBar } from './components/NavBar';
 import HomePage from './pages/HomePage';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import store, { persistor } from './components/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { AllPlayers } from './pages/Players';
@@ -20,42 +20,82 @@ import SignIn from './pages/SignIn';
 import LandingPage from './pages/LandingPage';
 import LeagueManagement from './pages/LeagueManagement'
 import TeamHub from './pages/TeamHub'
+import { FileTextOutlined } from '@ant-design/icons';
+import { FloatButton } from 'antd';
 
 const queryClient = new QueryClient();
 
+// Create a separate component for the float button that can access Redux state
+function HelpButton() {
+  const navigate = useNavigate();
+  // Access the league info from Redux store
+  const leagueInfo = useSelector((state) => state.league.currentLeague);
+  
+  const handleFloatButtonClick = () => {
+    navigate('/league');
+  };
+  
+  // Create a description based on league info
+  const description = leagueInfo ? `${leagueInfo.league_name || 'League Info'}` : 'HELP INFO';
+  
+  return (
+    <FloatButton
+      // icon={<FileTextOutlined />}
+      trigger="click"
+      onClick={handleFloatButtonClick}
+      type="primary"
+      tooltip={<div style={{ color: "white" }}>League Name</div>}
+      description={description}
+      shape="circle"
+      style={{ insetInlineEnd: 24, 
+        width: '60px',
+        height: '60px' }}
+      size="large"
+    />
+  );
+}
 
+// Main App component
 function App() {
   return (
-    
     <div>
-      
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <HashRouter>
-            <NavBar/>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/players" element={<AllPlayers />} />
-              {/* <Route path="/auction" element = {<Auction />}/> */}
-              <Route path="/auction" element = {<NewAuction />}/>
-              <Route path="/draft" element = {<SnakeDraft />}/>
-              <Route path="/manageteam" element = {<ManageTeam />}/>
-              <Route path="/teams" element = {<Teams />}/>
-              <Route path ="/teampoints" element = {<TeamPoints />} />
-              <Route path ="/linegraph" element = {<Linegraph />} />
-              <Route path="/waiver" element = {<WaiverSystem/>} />
-              <Route path="/SignIn" element = {<SignIn />} />
-              <Route path='/league' element={<LandingPage />} />
-              <Route path="/manageleague" element = {<LeagueManagement />}/>
-              <Route path="/teamhub" element = {<TeamHub />}/>
-            </Routes>
+              <AppContent />
             </HashRouter>
           </PersistGate>
         </Provider>
       </QueryClientProvider>
     </div>
+  );
+}
+
+// Create a separate component that will be rendered inside the Router
+function AppContent() {
+  return (
+    <>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/players" element={<AllPlayers />} />
+        <Route path="/auction" element={<NewAuction />}/>
+        <Route path="/draft" element={<SnakeDraft />}/>
+        <Route path="/manageteam" element={<ManageTeam />}/>
+        <Route path="/teams" element={<Teams />}/>
+        <Route path="/teampoints" element={<TeamPoints />} />
+        <Route path="/linegraph" element={<Linegraph />} />
+        <Route path="/waiver" element={<WaiverSystem/>} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path='/league' element={<LandingPage />} />
+        <Route path="/manageleague" element={<LeagueManagement />}/>
+        <Route path="/teamhub" element={<TeamHub />}/>
+      </Routes>
       
+      {/* HelpButton can now access both Router context and Redux state */}
+      <HelpButton />
+    </>
   );
 }
 
